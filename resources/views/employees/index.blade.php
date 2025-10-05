c7977f37dd6249c6cd0536c91c8561e9210a4894
cellayss <ashlihatulsotya@gmail.com>
2025-10-05 20:30:48 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Daftar Pegawai</title>
-</head>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
 
-<body>
 <div class="container mt-5">
     <h1 class="mb-4">Daftar Pegawai</h1>
     <table border="1" cellpadding="5" cellspacing="0">
@@ -44,5 +41,4 @@
         </tbody>
     </table>
 </div>
-</body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
-    return redirect()->route('employees.index');
+    return view('welcome');
 });
 
 Route::resource('employees', EmployeeController::class);
